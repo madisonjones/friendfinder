12ff8ec2a8081b4240384ab2ea41dfb9501e7642
Madison Jones <Madison Jones>
2017-09-01 09:59:41 -0400

    updates

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+
+    });
+
+    app.post("api/friends", function(req, res){
+
+    })
+}
